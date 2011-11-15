54c52eb3c0f9b27a2ef5a048ad767b0c9724488d
Timothy Lambert <consciousgamer@gmail.com>
2011-11-15 00:54:29 -0800

    Added GameMagazine prefab as basic platform.

---
+++ Assets/Scripts/Platforms/Platform.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+/**
+ * Basic surface that the character can run on.
+ * Can contain particle, sound, physics triggers
+ */
+public class Platform : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
