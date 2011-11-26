58bbf956663f849608a168cd8bff945fd95c3dd1
Oscar <Oscar@Oscar-THINK>
2011-11-26 02:29:46 -0800

    A little improvement to the movement of the Character Controller.

--- Assets/Scripts/Movement/CharacterControllerMovementScript.cs
+++ Assets/Scripts/Movement/CharacterControllerMovementScript.cs
@@ -8,55 +8,69 @@ public class CharacterControllerMovementScript : MonoBehaviour {
 	public float fallRate = 54f;
 	
 	private bool lookingLeft;
-	private bool hasJumped;
+	private bool isJumping;
+	private float lastInputValue;
 	private float notGroundedTimer;
-	private Vector3 moveDirection;
 	
 	// Use this for initialization
 	void Start () {
 		lookingLeft = true;
-		hasJumped = false;
+		isJumping = false;
+		lastInputValue = 0;
 		notGroundedTimer = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.LookAt(transform.position + (lookingLeft ? Vector3.left : Vector3.right), Vector3.up);
-		CharacterController controller = GetComponent<CharacterController>();
 		
-		moveDirection = Vector3.zero;
-		if (Input.GetAxis("Horizontal") > 0) {
-			animation.CrossFade("run_forward");
-			if (lookingLeft) {
+		// make character look in the right direction and not topple over
+		transform.LookAt(transform.position + (lookingLeft ? Vector3.left : Vector3.right), Vector3.up);
+		
+		// get variables
+		CharacterController controller = GetComponent<CharacterController>();
+		Vector3 moveVector = Vector3.zero;
+		
+		// check input status and do actions
+		if (Input.GetAxis("Horizontal") != 0) {
+			bool movingRight = (Input.GetAxis("Horizontal") > 0);
+			
+			if ((movingRight) ? lookingLeft : !lookingLeft) {
 				transform.FindChild("player_root").Rotate(new Vector3(0, 180, 0));
 			}
-			moveDirection.x += moveMultiplier;
-			lookingLeft = false;
-		} else if (Input.GetAxis("Horizontal") < 0) {
-			animation.CrossFade("run_forward");
-			if (!lookingLeft) {
-				transform.FindChild("player_root").Rotate(new Vector3(0, 180, 0));
+			lookingLeft = (movingRight) ? false : true;
+			
+			if ((movingRight && lastInputValue <= Input.GetAxis("Horizontal")) ||
+			    (!movingRight && lastInputValue >= Input.GetAxis("Horizontal"))) {
+				animation.CrossFade("run_forward");
+				moveVector.x += ((movingRight) ? 1 : -1) * moveMultiplier * Time.deltaTime;
+				lastInputValue = Input.GetAxis("Horizontal");
+			} else {
+				animation.CrossFade("idle");
+				lastInputValue = 0;
 			}
-			moveDirection.x += -moveMultiplier;
-			lookingLeft = true;
+			
 		} else {
 			animation.CrossFade("idle");
 		}
 		
+		// reset acceleration if on ground
 		if (controller.isGrounded) {
 			notGroundedTimer = 0;
-			hasJumped = false;
+			isJumping = false;
 		}
-			
-		if (controller.isGrounded && Input.GetAxis("Vertical") > 0){ // only jump from the ground
-			hasJumped = true;
+		
+		// only jump from the ground
+		if (controller.isGrounded && Input.GetAxis("Vertical") > 0){
+			isJumping = true;
 	    }
 		
-		if (hasJumped) {
-			moveDirection.y += jumpSpeed * Time.deltaTime;
+		// if jumping, add the jumpspeed to current movement
+		if (isJumping) {
+			moveVector.y += jumpSpeed * Time.deltaTime;
 		}
+		// increase velocity (accelerate downward, ie. gravity)
 		notGroundedTimer += Time.deltaTime;
-		moveDirection.y -= fallRate * notGroundedTimer*notGroundedTimer;
-		controller.Move(moveDirection * Time.deltaTime);
+		moveVector.y -= fallRate * notGroundedTimer*notGroundedTimer;
+		controller.Move(moveVector * Time.deltaTime);
 	}
 }
