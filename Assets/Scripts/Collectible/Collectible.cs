046f7e6631897cc1d05b66282c3e130f6e0a13a7
Timothy Lambert <consciousgamer@gmail.com>
2011-12-03 23:12:16 -0800

    1) Added killzone to reload level when player falls off screen 2) Add audio place holders to collectible trigger.

--- Assets/Scripts/Collectible/Collectible.cs
+++ Assets/Scripts/Collectible/Collectible.cs
@@ -5,9 +5,14 @@ public class Collectible : MonoBehaviour {
 	
 	// Set through editor
 	public GameObject collectItemFX = null;
+	
+	// Keep ref to audio source attached
+	private AudioSource collectibleHit;
 
 	// Use this for initialization
 	void Start () {		
+		
+		InitializeAudio();
 	}
 	
 	// Update is called once per frame
@@ -22,14 +27,26 @@ public class Collectible : MonoBehaviour {
 		
 		// Fire Particle FX
 		if( collectItemFX )
-		{
-			print("Fire particles");
+		{			
 			GameObject.Instantiate( collectItemFX, this.transform.position, this.transform.rotation );			
 		}
 		
-		// Play Sound FX			
+		// Play Sound FX	
+		AudioSource.PlayClipAtPoint( collectibleHit.clip, Vector3.zero );
 		
 		// Destroy this collectible
 		GameObject.Destroy( this.gameObject );
+	}
+	
+	void InitializeAudio()
+	{
+		// Create references to the attached audio sources.
+		foreach( AudioSource source in this.GetComponents<AudioSource>() )
+		{
+			if( source.clip.name == "CollectibleHit" )
+			{
+				collectibleHit = source;				
+			}			
+		}			
 	}
 }
---
+++ Assets/Scripts/LevelObjects/Killzone.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof( Collider ))]
+
+/// <summary>
+/// Player is "dead" if it hits any part of the killzone.
+/// </summary>
+public class Killzone : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerEnter( Collider other )
+	{		
+		// Play sound
+		
+		// Play particle
+		
+		// Reset the level or spawn the player at the last check point
+		
+		
+		// TODO DEBUG ONLY
+		LevelManager.instance.FinishLevel();
+	}
+}
